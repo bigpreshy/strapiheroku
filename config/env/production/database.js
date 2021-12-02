894250e7380bfd37be4f839366ff982031c0c7e6
Preshy <lukeprecious@gmail.com>
2021-12-02 20:25:53 +0100

    Added some config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -8,10 +8,8 @@ module.exports = ({ env }) => ({
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
         schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-        },
+        ssl: env('DATABASE_SSL', false)
       },
-      debug: false,
+     // debug: false,
     },
   });
