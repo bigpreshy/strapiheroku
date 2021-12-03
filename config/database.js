73238fd0c6876b16d963f28a2bb3a3aafcbeb6c8
Preshy <lukeprecious@gmail.com>
2021-12-03 06:10:41 +0100

    added heroku url

--- config/database.js
+++ config/database.js
@@ -1,16 +1,11 @@
+const path = require('path');
+
 module.exports = ({ env }) => ({
-  connector: "bookshelf",
+  connection: {
+    client: 'sqlite',
     connection: {
-      client: 'postgres',
-      settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        schema: env('DATABASE_SCHEMA', 'public'), // Not required
-        ssl: env('DATABASE_SSL', false)
-      },
-     // debug: false,
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-  });
+    useNullAsDefault: true,
+  },
+});
